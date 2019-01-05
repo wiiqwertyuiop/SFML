31f235724de00361daede4005757ad116d9ac823
wiiqwertyuiop <33212768+wiiqwertyuiop@users.noreply.github.com>
2019-01-04 20:07:55 -0500

    Added insert and delete functionality

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -5,12 +5,13 @@
 /*
 
 TODO:
- - Insert text at cursor
- - Word wrap
- - Up/Down arrows
- - Done
+-  Word wrap
+-  Up/Down arrows
+-  Done
 
  Done:
+- Insert text at cursor
+- Deleting works; including cursor position
 - Left/Right arrows
 
  */
@@ -59,31 +60,43 @@ int main() {
 
 				if (Event.key.code == sf::Keyboard::Escape) Window.close();
 				else if (Event.key.code == sf::Keyboard::Enter) {
-					sentence += "\n";
+					
+					// Position text cursor on new line
 					cursorPos.x = fontSize / 10;
 					cursorPos.y += fontSize;
-					cursorTracker++;
-				}
-				else if (Event.key.code == sf::Keyboard::BackSpace && sentence.getSize() > 0) {				
 
-					if (text.findCharacterPos(sentence.getSize() - 1).x + (fontSize / 2) >= screenDimensions.x) {
-						// If we backspace and we end up off screen delete the '\n' and the character before it and wrap around
-						sentence.erase(sentence.getSize() - 1, 1);
+					// Insert new line
+					sentence.insert(cursorTracker, '\n');
+					text.setString(sentence);
+					cursorTracker++;
+
+				}
+				else if (Event.key.code == sf::Keyboard::BackSpace && sentence.getSize() > 0 && cursorTracker != 0) {
+
+					// Delete character before cursor position
+					int deletePos = cursorTracker - 1;
+
+					if (text.findCharacterPos(deletePos).x + (fontSize / 2) >= screenDimensions.x) {
+						// If we backspace and we end up off screen delete the new line character as well as the other character and loop us around
+						sentence.erase(deletePos, 1);
+						//text.setString(sentence);
+						cursorTracker--;
 						cursorPos.y -= fontSize;
+						//break;
 					}
 					// If we are at the beginning of a new line or there is a new line charcter before us wrap around
-					else if (cursorPos.x == fontSize / 10 || sentence[sentence.getSize() - 1] == '\n') cursorPos.y -= fontSize;
+					else if (cursorPos.x == fontSize / 10 || sentence[deletePos] == '\n') cursorPos.y -= fontSize;
 
 					// Set the cursor's X position to the character's that will be erased
-					cursorPos.x = text.findCharacterPos(sentence.getSize() - 1).x;
+					cursorPos.x = text.findCharacterPos(deletePos).x;
 
-					sentence.erase(sentence.getSize() - 1, 1);
+					sentence.erase(deletePos, 1);
 					text.setString(sentence);
 
-					// If we are on an empty line, position the cursor at the begining of the line
-					if (sentence.getSize() > 0 && sentence[sentence.getSize() - 1] == '\n') cursorPos.x = fontSize / 10;
-
 					cursorTracker--;
+
+					// If we are on an empty line, position the cursor at the begining of the line
+					if (cursorTracker != 0 && sentence.getSize() > 0 && sentence[deletePos] == '\n') cursorPos.x = fontSize / 10;
 
 				}
 				else if (Event.key.code == sf::Keyboard::Left && cursorTracker > 0) {
@@ -140,7 +153,7 @@ int main() {
 				if (Event.text.unicode >= 32 && Event.text.unicode <= 126) {
 					
 					// Add new character to sentence
-					sentence += (char)Event.text.unicode;
+					sentence.insert(cursorTracker, (char)Event.text.unicode);
 					text.setString(sentence);
 
 					// Increase crusor pos by width of the character
